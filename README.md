77e9c89b4519ccd1ffb608d64306114bf7c85e81
Philipp Serr <episource@gmx.de>
2016-05-06 22:05:51 +0200

    Set mtime based on the history of the whole site
    By assigning `mtime: site` in the yaml front matter of a page, the time
    of last change will be calculated based on the history of the whole
    site instead of the history of the current page.

--- README.md
+++ README.md
@@ -1,31 +1,49 @@
 # jekyll-git-mtime plugin
 
-A Jekyll plugin that sets the page variable `page.mtime` to the time of the last commit -or- if uncommited changes exist to the file's mtime. By explicitly setting `mtime: build` or `mtime: site.time` in a page's yaml frontmatter, the `page.mtime` variable can also be explicitly set to the current time (retrieved via [site.time](https://jekyllrb.com/docs/variables/)).
-
+A Jekyll plugin that sets the page variable `page.mtime` to the time of the last commit or change.
+ 
 ## Prerequisites
 The git executable must be found within PATH.
 
+## Primary Usecase
+Display the time of the last change in the page footer. Usually the time of last change shall refer to the current page, only. Regarding special pages like index or sitemap, the time of last change regarding any page shall be used instead.
+
 ## Usage
+#### Set page.mtime based on the history of the current page
+Per default  `page.mtime` is set to the time of the last commit related to the current page or to the page's file mtime if there are uncommited changes regarding the page.
 
 Within any page:
 ```liquid
 {{ page.mtime }}
 ```
-    
+
+#### Format date
 The liquid filter [date](https://help.shopify.com/themes/liquid/filters/additional-filters#date) maybe used for formatting:
 ```liquid
 {{ page.mtime | date: '%Y-%m-%d' }}
 ```
-    
-Setting `mtime: build` or `mtime: site.time` in the yaml frontmatter sets `page.mtime` to `site.time`:
+
+#### Set page.mtime based on the history of the whole site
+By specifying `mtime: site` or `mtime: branch` the plugin is configured to update the `mtime`-value based on the history of the whole site. This means that `page.mtime` is set to the time of the last commit (regarding the current branch & the whole site) or to the instant of the current build ([site.time](https://jekyllrb.com/docs/variables/)) if there are uncommited changes.
+```liquid
+---
+mtime: site
+---
+{{ page.mtime }}
+```
+
+
+#### Set page.mtime to the instant of the current build (site.time)
+`page.mtime` can be set to the the instant of the current build ([site.time](https://jekyllrb.com/docs/variables/)) by specifying `mtime: build` or `mtime: site.time` in the yaml frontmatter.
 ```liquid
     ---
-    mtime: site.time
+    mtime: build
     ---
     {{ page.mtime | date: '%Y-%m-%d' }}=={{ site.time | date: '%Y-%m-%d' }}
-```
-    
-Any other value assigned to `page.mtime` in the the yaml frontmatter is retained:
+``` 
+  
+#### Retain other values assigned to page.mtime    
+Any other value assigned to `page.mtime` other than the keywords explained above is retained:
 ```liquid
 ---
 mtime: something
--- jekyll-git-mtime.rb
+++ jekyll-git-mtime.rb
@@ -10,29 +10,48 @@ module Jekyll
         # Various constants
         MTIME_PAGE_VAR = 'mtime'
 
+        def self.get_mtime(path=nil)
+            additional_status_options = ''
+            additional_log_options = ''
+
+            if (path != nil)
+                additional_status_options = "--ignored -- #{path}"
+                additional_log_options = "-- #{path}"
+            end
+
+            git_status_str = `git status --porcelain -uall #{additional_status_options}`
+            if ($? != 0)
+                raise GitError, "git status failed"
+            end
+
+            if (!git_status_str.empty?)
+                # git status reported that the file or workspace has been modified/ignored/staged/...
+                return nil
+            else
+                git_log_str = `git log -1 --format=format:"%ct" #{additional_log_options}`
+                if ($? != 0 || git_log_str.empty?)
+                    raise GitError, "git log failed"
+                end
+
+                return Time.at git_log_str.to_i
+            end
+        end
+
         Jekyll::Hooks.register(:pages, :post_init, priority: LOW_PRIORITY) do |page|
-            if (page.data[MTIME_PAGE_VAR] == 'site.time' || page.data[MTIME_PAGE_VAR] == 'build')
+            puts page.name
+            puts page.data[MTIME_PAGE_VAR] || "empty"
+            if (page.data[MTIME_PAGE_VAR] == 'build' || page.data[MTIME_PAGE_VAR] == 'site.time')
                 # mtime has been explicitly set to site.time (time of current build)
                 page.data[MTIME_PAGE_VAR] = page.site.time
+            elsif (page.data[MTIME_PAGE_VAR] == 'site' || page.data[MTIME_PAGE_VAR] == 'branch')
+                # set mtime to the time of the latest of any commits (not necessarily related to the current page“
+                page.data[MTIME_PAGE_VAR] = get_mtime || page.site.time
             elsif (page.data[MTIME_PAGE_VAR] == nil)
-                git_status_str = `git status --porcelain -uall --ignored -- #{page.path}`
-                if ($? != 0)
-                    raise GitError, "git status failed"
-                end
-
-                if (!git_status_str.empty?)
-                    # git status reported that the file has been modified/ignored/staged/...
-                    # => report file mtime instead of the last commit's time
-                    page.data['mtime'] = File.mtime page.path
-                else
-                    git_log_str = `git log -1 --format=format:"%ct" -- #{page.path}`
-                    if ($? != 0 || git_log_str.empty?)
-                        raise GitError, "git log failed"
-                    end
-
-                    page.data['mtime'] = Time.at git_log_str.to_i
-                end
+                # set mtime to the time of the last commit related to the current page
+                page.data[MTIME_PAGE_VAR] = get_mtime(page.path) || File.mtime(page.path)
             end
+            puts page.data[MTIME_PAGE_VAR]
+            puts ""
         end
     end
 end
